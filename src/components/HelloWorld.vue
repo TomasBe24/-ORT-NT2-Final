4937543db05a93bc4d22a557a0ab5e40715e492a
TomasBe24 <tomas.berias1@hotmail.com>
2022-07-12 20:15:58 -0300

    Finalizado

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,83 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+
+    Color: <input type="text" v-model="color">
+    Fondo: <input type="text" v-model="fondo">
+    Size: <input type="text" v-model="size">
+    <hr>
+
+    Texto: <input type="text" v-model="texto"
+    :style="getStyle">
+
+    {{ texto }}
+
+    <palabras v-show="texto " :texto="texto"  />
+
+    <table>
+      <tr>
+        <th>Pregunta</th>
+        <th>Respuesta</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>C</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>D</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>D</td>
+      </tr>
+    </table> 
+
   </div>
+   
 </template>
 
 <script>
+
+import palabras from './palabras.vue'
 export default {
   name: 'HelloWorld',
+  components: {
+    palabras
+  },
   props: {
-    msg: String
+    
+  },
+  data() {
+    return{
+      texto : "",
+      color : '',
+      fondo : '',
+      size : ''
+    }
+  },
+  methods: {
+  },
+  computed: {
+    getStyle(){
+      return { 
+        'color': this.color,
+        'background-color': this.fondo,
+        'font-size': this.size
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
---
+++ src/components/palabras.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <section class="src-components-palabras">
+    <br>
+    Palabras: {{getPalabras}}
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-palabras',
+    props: ['texto'],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      getPalabras(){
+        let arrayPalabras = this.texto.trim().split(' ')
+        let cantPalabras = arrayPalabras.length
+
+        return cantPalabras
+      }
+    }
+}
+
+
+</script>
+
+<style scoped>
+  
+</style>
